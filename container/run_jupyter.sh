69e516665a23cba579a6e4f8a4ea54f63a9280c1
gucchi0224 <shunsuke.hs0224@gmail.com>
2025-11-24 02:28:47 +0000

    jupyter lab起動コマンド

---
+++ container/run_jupyter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script to start Jupyter notebook inside the Docker container
+
+# Start Jupyter notebook
+jupyter lab \
+    --ip=0.0.0.0 \
+    --port=8888 \
+    --no-browser \
+    --allow-root \
+    --NotebookApp.token='' \
+    --NotebookApp.password='' \
+    --notebook-dir=/workspace/sam3
